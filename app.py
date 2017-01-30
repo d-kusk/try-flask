edc32fac3d939f839216ba2c5783416ea2a86d37
Daisuke Konishi <skydai1151@gmail.com>
2017-01-31 01:26:28 +0900

    URLビルドの確認

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "Hello World"
+def index():
+    "Index page."
+
+
+@app.route('/login')
+def login(): pass
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    'hello {name}'.format(**locals())
+
+
+@app.route('/usr/<username>')
+def profile(username): pass
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 
 if __name__ == '__main__':
