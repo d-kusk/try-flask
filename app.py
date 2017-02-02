a2c20000b5598796cdf6a02cb2abde65a6aefb68
Daisuke Konishi <skydai1151@gmail.com>
2017-02-03 00:49:46 +0900

    helloにアクセスしてきたユーザーを取得してjinja2で表示

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    "Index page."
+    return ('index')
 
 
-@app.route('/login')
-def login(): pass
-
-
+@app.route('/hello/')
 @app.route('/hello/<name>')
-def hello(name):
-    'hello {name}'.format(**locals())
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 
-@app.route('/usr/<username>')
-def profile(username): pass
+# @app.route('/login')
+# def login(): pass
+#
+#
+# @app.route('/user/<username>')
+# def profile(username): pass
 
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John Doe'))
 
 
 if __name__ == '__main__':
