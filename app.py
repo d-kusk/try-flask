13632e9e8724aae65851a97405ce5457aed86c0c
Daisuke Konishi <skydai1151@gmail.com>
2017-03-07 01:33:13 +0900

    テンプレートのレンダリングまでやった

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return ('index')
+@app.route('/index')
+def index(): pass
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.methods == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
 
 
 @app.route('/hello/')
@@ -15,20 +23,15 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-# @app.route('/login')
-# def login(): pass
-#
-#
-# @app.route('/user/<username>')
-# def profile(username): pass
+@app.route('/user/<username>')
+def profile(username): pass
 
 
 # with app.test_request_context():
-#     print(url_for('index'))
-#     print(url_for('login'))
-#     print(url_for('login', next='/'))
-#     print(url_for('profile', username='John Doe'))
-
+#     print url_for('index')
+#     print url_for('login')
+#     print url_for('login', next='/')
+#     print url_for('profile', username='John Doe')
 
 if __name__ == '__main__':
     app.run(
